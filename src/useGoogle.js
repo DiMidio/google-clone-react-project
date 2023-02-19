bc184ed586801f252f3a9c32a05f62903e5d7162
dimidio <dmdimidio@gmail.com>
2023-02-19 02:29:24 -0500

    google search api

--- src/useGoogle.js
+++ src/useGoogle.js
@@ -4,7 +4,7 @@ const useGoogle = term => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_API_KEY}&cx=${process.env.REACT_APP_CONTEXT_KEY}&q=${term}`)
+        fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyDkzWlBhKRkcSm0JOdReJJ5d3AOasTfRGI&cx=b4344e2dbe28d4577&q=${term}`)
             .then((response) => response.json()).then((result) => {
                 setData(result);
             })
